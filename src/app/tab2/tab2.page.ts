7f9c34895b65fae030821e973154a51c86ac760b
LuksAl123 <lucasalprogrammer@gmail.com>
2024-12-24 19:35:25 -0300

    Sixth Commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { lastValueFrom, Observable, Subscription } from 'rxjs';
+import { IonItemSliding } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
+import { lastValueFrom, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
@@ -10,12 +13,17 @@ import { lastValueFrom, Observable, Subscription } from 'rxjs';
 })
 export class Tab2Page implements OnInit, OnDestroy {
 
+  // @ViewChild('itemSliding') itemSliding!: IonItemSliding;
   posts: any[] = [];
   // posts!: Observable<any>;
   postsSub!: Subscription;
   private http = inject(HttpClient);
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    addIcons({
+      add, 
+    });
+  }
 
   ngOnInit() {
     this.fetchPosts();
@@ -42,6 +50,61 @@ export class Tab2Page implements OnInit, OnDestroy {
   navigateToPost(post: any) {
     console.log(post);
     this.router.navigate(['/', 'tabs', 'tab2', 'posts', post?.id]);
+  }
+
+  async createPost() {
+    try {
+      const result = await lastValueFrom(
+        this.http.post<any>('https://jsonplaceholder.typicode.com/posts', {
+          title: 'Test post',
+          body: 'This is a test post',
+          userId: '1',
+        })
+      );
+      console.log(result);
+
+      this.posts = [{...result}, ...this.posts];
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  async deletePost(post: any, itemSliding: IonItemSliding) {
+    try {
+      await lastValueFrom(
+        this.http.delete<any>(
+          `https://jsonplaceholder.typicode.com/posts/${post?.id}`
+          )
+      );
+
+      itemSliding.close();
+      this.posts = this.posts.filter((pst) => pst?.id !== post?.id);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  async updatePost(post: any, itemSliding: IonItemSliding, index: number) {
+    try {
+      const result = await lastValueFrom(
+        this.http.patch<any>(`https://jsonplaceholder.typicode.com/posts/${post?.id}`, {
+          title: 'Test post',
+          body: 'This is a test post',
+        })
+        // this.http.patch<any>(`https://jsonplaceholder.typicode.com/posts/${post?.id}`, {
+        //   title: 'Test post',
+        //   body: 'This is a test post',
+        //   userId: post?.userId,
+        // })
+      );
+      console.log(result);
+
+      itemSliding.close();
+
+      this.posts[index] = result;
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   ngOnDestroy() {
